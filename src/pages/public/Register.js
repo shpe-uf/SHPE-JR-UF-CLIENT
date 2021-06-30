2993727ea711909a047f87e418f2e9aa5a43d0fd
MarianaTorresTorres <mari.torret@gmail.com>
2021-06-30 14:26:59 -0400

    auth route established

--- src/pages/public/Register.js
+++ src/pages/public/Register.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { gql, useMutation } from "@apollo/client";
 import { Button, Form } from "semantic-ui-react";
 
+import { AuthContext } from "../../context/auth";
 import { useForm } from "../../util/hooks";
 
 function Register(props) {
+  const context = useContext(AuthContext);
   const [errors, setErrors] = useState({});
 
   const { onChange, onSubmit, values } = useForm(registerUser, {
@@ -17,7 +19,8 @@ function Register(props) {
   });
 
   const [addUser, { loading }] = useMutation(REGISTER_USER, {
-    update(_, result) {
+    update(_, { data: { register: userData } }) {
+      context.login(userData);
       props.history.push("/");
     },
     onError(err) {
