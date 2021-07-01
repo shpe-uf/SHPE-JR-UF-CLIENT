816d0056187dda08f00fe32b7cf230a1e12d85da
MarianaTorresTorres <mari.torret@gmail.com>
2021-07-01 02:55:10 -0400

    connect to heroku server

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import {
 } from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://git.heroku.com/shpe-jr-uf.git",
 });
 
 const client = new ApolloClient({
